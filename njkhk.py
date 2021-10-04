0c1bc8eea908b789e2e5e72eb37b951445663486
Jsschneider23 <91565732+Jsschneider23@users.noreply.github.com>
2021-10-04 15:02:45 -0500

    Add files via upload

---
+++ njkhk.py
@@ -0,0 +1,14 @@
+#Initialiation
+import turtle
+import random
+t=turtle.Turtle()
+#Functions
+def randomDot():
+    t.color("red")
+    t.begin_fill()
+    t.circle(100)
+    t.end_fill()
+    
+#Main
+randomDot()
+
